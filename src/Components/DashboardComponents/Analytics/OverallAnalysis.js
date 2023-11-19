8230b85b1c101136610b2e388a1adc65224fca70
milangupta95 <milangupta95@gmail.com>
2023-11-19 17:21:14 +0530

    General Setup

--- src/Components/DashboardComponents/Analytics/OverallAnalysis.js
+++ src/Components/DashboardComponents/Analytics/OverallAnalysis.js
@@ -29,29 +29,50 @@ function OverallAnalysis() {
     return (
         <div className='flex items-center flex-col justify-center space-y-8'>
             <div className='h-[500px] w-[95%] p-4 shadow-sm '>
-                <h1 className='text-2xl font-bold'>Conversion Analysis</h1>
+                <div className='flex items-center justify-between'>
+                    <h1 className='text-2xl font-bold'>Conversion Analysis</h1>
+                    <div className='w-[50%] flex justify-center items-center space-x-8'>
+                        <div className='flex space-x-2 items-center'>
+                            <div className='w-[16px] h-[16px] bg-[#6E4D82]'></div>
+                            <p>Footfall</p>
+                        </div>
+                        <div className='flex space-x-2 items-center'>
+                            <div className='w-[16px] h-[16px] bg-[#8969AD]'></div>
+                            <p>Engagers</p>
+                        </div>
+                        <div className='flex space-x-2 items-center'>
+                            <div className='w-[16px] h-[16px] bg-[#A69EC6]'></div>
+                            <p>Group Count</p>
+                        </div>
+                        <div className='flex space-x-2 items-center'>
+                            <div className='w-[16px] h-[16px] bg-[#C9C4DF]'></div>
+                            <p>Conversion</p>
+                        </div>
+                    </div>
+                </div>
+
                 <ResponsiveFunnel
                     data={data}
                     margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
                     valueFormat=">-.4s"
-                    colors={{ scheme: 'spectral' }}
-                    borderWidth={20}
+                    direction='horizontal'
+                    colors={{ scheme: 'purple_orange' }}
+                    fillOpacity={0.7}
+                    borderWidth={5}
+                    borderColor={{ from: 'color', modifiers: [] }}
+                    borderOpacity={1}
                     labelColor={{
                         from: 'color',
                         modifiers: [
                             [
                                 'darker',
-                                3
+                                '3'
                             ]
                         ]
                     }}
-                    direction='horizontal'
-                    beforeSeparatorLength={100}
-                    beforeSeparatorOffset={20}
-                    afterSeparatorLength={100}
-                    afterSeparatorOffset={20}
-                    currentPartSizeExtension={10}
-                    currentBorderWidth={40}
+                    beforeSeparatorLength={50}
+                    afterSeparatorLength={50}
+                    currentBorderWidth={0}
                     motionConfig="wobbly"
                 />
             </div>
